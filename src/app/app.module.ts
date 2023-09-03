c14b09970e0eb4febe03068ff3a2e99fa7c0f9ed
hughknaus <37626121+hughknaus@users.noreply.github.com>
2023-09-02 18:31:12 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './../environments/environment';
-import { reducer } from './store/reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { PhotosEffects } from './store/effects';
 
